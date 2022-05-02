4b57e0946b2403d2380cfb13bd0c2c9838c61433
Wenyun Wang <wwang@g.harvard.edu>
2022-05-01 21:31:40 -0400

    new

--- speckle/inference/correlation_heatmap.py
+++ speckle/inference/correlation_heatmap.py
@@ -8,7 +8,6 @@ def get_correlation(tauidx,loadtRange,t_range, px, corr_maps): #return seconds
     sum_correlation=0
     for t in range(0,len(t_range),1):
         sum_correlation=sum_correlation+px_corrmap[tauidx,t]
-        print(px_corrmap[tauidx,t])
     average_correlation=sum_correlation/len(t_range)
     return average_correlation
 
@@ -27,4 +26,44 @@ def display_correlation_heatmaps(lag, numberT_average, minV, maxV, foldername):
     ax = sns.heatmap(heatmap_lag,vmin=minV, vmax=maxV)
     plt.show()
     
-#def plot_radius_distribution()
+def mask_heatmap(center,radius_out,heatmap, scale_bar_position, scale_ratio):
+    ##scale_bar_position:[x,y]
+    x_center=center[1]
+    y_center=center[0]
+    x = np.arange(heatmap.shape[0])
+    y = np.arange(heatmap.shape[1])
+    xv, yv = np.meshgrid(x,y)
+    xv -= x_center
+    yv -= y_center
+    radius = np.sqrt(xv**2 + yv**2)
+    mask=radius<radius_outer
+    heatmap_masked=heatmap*mask
+    f, ax = plt.subplots(figsize=(10, 8))
+    ax = sns.heatmap(heatmap_masked,vmin=0, vmax=0.7)
+    ## scale bar
+    ax.plot([scale_bar_position[0],scale_bar_position[0]+5*scale_ratio],[scale_bar_position[1],scale_bar_position[1]],'w-')
+    ax.text(scale_bar_position[0]+20,scale_bar_position[1]+10,r'$5\mu m$',color='w')
+    plt.show()
+    plt.show()
+    return heatmap_masked
+
+def plot_radius_distribution(heatmap,radius_out,scale_ratio):
+    x = np.arange(heatmap.shape[0])
+    y = np.arange(heatmap.shape[1])
+    xv, yv = np.meshgrid(x,y)
+    xv -= x_center
+    yv -= y_center
+    radius = np.sqrt(xv**2 + yv**2)
+    step=1
+    lower_bounds=np.arange(0,radius_out,step)
+    micron_list=(lower_bounds+step/2)/scale_ratio ## mark with middle position, so step/2
+    intensity_r=[]
+    for bound in lower_bounds:
+        bound_up=bound+step
+        mask = np.logical_and(radius >= bound, radius <= bound_up)
+        avg_intensity=np.sum(heatmap*mask)/np.sum(mask)
+        intensity_r.append(avg_intensity)
+    intensity_r=np.array(intensity_r)
+    print(intensity_r)
+    plt.plot(micron_list,intensity_r,'.')
+    #plt.ylim(0,0.3)
